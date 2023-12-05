9c1ca544ffb0de3982c5c574f816ace901961e4d
Manuel <manuel@strenge.ch>
2023-12-05 21:48:11 +0100

    Part two Day 5 no time to wait, just brut force that shit in parallel

--- manuel/src/day5/main.go
+++ manuel/src/day5/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"common"
+	"math"
 	"strconv"
 	"strings"
+	"sync"
 	"unicode"
 )
 
@@ -11,36 +13,59 @@ var tables = make([][]RangeMap, 0)
 
 type RangeMap struct {
 	source      int
-	desitnation int
+	destination int
 	width       int
 }
 
-func newRangeMap(s string) RangeMap {
+func (m RangeMap) isInRange(x int) bool {
+	if x >= m.source && x < m.source+m.width {
+		return true
+	}
+	return false
+}
 
+func (m RangeMap) findDestination(x int) int {
+	if m.isInRange(x) {
+		return (x - m.source) + m.destination
+	}
+	return -1
+}
+
+func newRangeMap(s string) RangeMap {
 	parts := strings.Split(s, " ")
 
-	source, err := strconv.Atoi(parts[0])
-	desitnation, err := strconv.Atoi(parts[1])
+	source, err := strconv.Atoi(parts[1])
+	destination, err := strconv.Atoi(parts[0])
 	width, err := strconv.Atoi(parts[2])
 
 	common.Check(err)
 
 	return RangeMap{
 		source:      source,
-		desitnation: desitnation,
+		destination: destination,
 		width:       width,
 	}
 }
 
 func readStartValues(s string) []int {
-	numberstring := strings.TrimSpace(strings.Split(s, ":")[1])
+	numberString := strings.TrimSpace(strings.Split(s, ":")[1])
 	numbers := make([]int, 0)
-	for _, v := range strings.Split(numberstring, " ") {
-		num, err := strconv.Atoi(string(v))
+	for _, v := range strings.Split(numberString, " ") {
+		num, err := strconv.Atoi(v)
 		common.Check(err)
 		numbers = append(numbers, num)
 	}
 	return numbers
+}
+
+func getDestination(x int, level int) int {
+	for _, m := range tables[level] {
+		r := m.findDestination(x)
+		if r != -1 {
+			return r
+		}
+	}
+	return x
 }
 
 func main() {
@@ -51,13 +76,64 @@ func main() {
 
 	for _, v := range lines[1:] {
 		if strings.Contains(v, ":") {
-			//TODO add unmapped numbers
 			instructionType++
-			tables = append(tables, make([]RangeMap,0 ))
+			tables = append(tables, make([]RangeMap, 0))
 		} else if len(v) > 0 && unicode.IsDigit(rune(v[0])) {
 			r := newRangeMap(v)
 			tables[instructionType] = append(tables[instructionType], r)
 		}
 	}
-	println(seeds)
+	res := make([]int, 0)
+
+	for _, seed := range seeds {
+		idx := seed
+		level := 0
+		for level != len(tables) {
+			idx = getDestination(idx, level)
+			//fmt.Printf("level: %d, idx: %d\n", level, idx)
+			level++
+		}
+		res = append(res, idx)
+
+	}
+	lowest := math.MaxInt
+
+	for _, v := range res {
+		if v < lowest {
+			lowest = v
+		}
+	}
+	//	println(lowest)
+
+	//flattenedMap := convertToFlatMap(tables)
+
+	lowest = math.MaxInt
+	l := sync.Mutex{}
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < len(seeds); i += 2 {
+		wg.Add(1)
+		i := i
+		go func() {
+
+			for x := seeds[i]; x < seeds[i]+seeds[i+1]; x++ {
+				idx := x
+				//println(x)
+				level := 0
+				for level != len(tables) {
+					//fmt.Printf("level: %d, idx: %d\n", level, idx)
+					idx = getDestination(idx, level)
+					level++
+				}
+				if idx < lowest {
+					l.Lock()
+					lowest = idx
+					l.Unlock()
+				}
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	println(lowest)
 }
