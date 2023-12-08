37a5ea827abf35daf8bf1cc8a61d82ec0c713674
Andreas <knexli7@gmail.com>
2023-12-08 11:30:52 +0100

    day 8 java

--- andreas/src/day8/Solution.java
+++ andreas/src/day8/Solution.java
@@ -42,7 +42,7 @@ public class Solution {
         return i;
     }
 
-    private static int solve2(Scanner sc) {
+    private static long solve2(Scanner sc) {
         char[] lr = sc.nextLine().toCharArray();
         sc.nextLine();
         Map<String, Node> adj = new HashMap<>();
@@ -58,45 +58,52 @@ public class Solution {
             adj.put(n.name, n);
         }
 
-        int i = 0, n = lr.length;
+        int i, n = lr.length;
         List<Node> currNodes = adj.entrySet().stream().filter(e -> e.getKey().endsWith("A"))
                 .map(Map.Entry::getValue)
                 .collect(Collectors.toList());
         System.out.println("amount starting nodes: " + currNodes.size() +
                 ", " + currNodes.stream().map(e -> e.name).toList());
-        Node[] nodes = new Node[currNodes.size()];
-        for (int j = 0; j < currNodes.size(); j++) {
-            nodes[j] = currNodes.get(j);
-        }
-        Set<String> visited = new HashSet<>();
-        while (!isDestination(nodes)) {
-            for (int j = 0; j < nodes.length; j++) {
+
+
+        // cycleTimes[i][j] = if i == 0: amount of steps after which first node with suffix Z with j-th start is reached
+        //                    if i == 1: cycle time to reach that same node again
+        int[][] cycleTimes = new int[2][currNodes.size()];
+        for (int k = 0; k < currNodes.size(); k++) {
+            Node start = currNodes.get(k);
+            Node currNode = start;
+            int c = 0;
+            i = 0;
+            while(true) {
                 if (lr[i % n] == 'L') {
-                    nodes[j] = adj.get(nodes[j].l);
+                    currNode = adj.get(currNode.l);
                 } else {
-                    nodes[j] = adj.get(nodes[j].r);
+                    currNode = adj.get(currNode.r);
+                }
+                i++;
+                if (currNode.name.endsWith("Z")) {
+                    if (c == 0) {
+                        cycleTimes[0][k] = i;
+                        c = i;
+                    } else {
+                        cycleTimes[1][k] = i - c;
+                        break;
+                    }
                 }
             }
-            String s = Arrays.stream(nodes).map(e -> e.name).reduce(String::concat).orElseThrow();
-            if (visited.contains(s)) {
-                System.out.println("Ran in cycle after iteration " + i);
-                break;
-            }
-            visited.add(s);
-            if (i % 10_000_000 == 0) {
-                System.out.println("Reached iteration " + i);
-            }
-            i++;
         }
-        return i;
-    }
 
-    private static boolean isDestination(Node[] nodes) {
-        for (Node n : nodes) {
-            if (!n.name.endsWith("Z")) {
-                return false;
+        long z = 0;
+        outer: while (true) {
+            // assume possible with z loops of the first startpos
+            long totalsteps = cycleTimes[0][0] + z * cycleTimes[1][0];
+            for (int k = 1; k < currNodes.size(); k++) {
+                if ((totalsteps - cycleTimes[0][k]) % cycleTimes[1][k] != 0) {
+                    z++;
+                    continue outer;
+                }
             }
+            return totalsteps;
         }
-        return true;
     }
 }
